31256c28dcaf63e8a4a15cc17773eda1f7367524
Markus Gaisbauer <markus.gaisbauer@dynatrace.com>
2023-05-30 22:20:44 +0200

    Use wunderbaum in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
+import {useEffect} from 'react';
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './App.css'
+
+import {Wunderbaum} from 'wunderbaum'
+
+function createFibonacciTree(n: number): any {
+  if (n < 2) {
+    return { title: "Node " + n }
+  }
+  return {
+    title: "Node " + n, expanded: true, children: [
+      createFibonacciTree(n - 1),
+      createFibonacciTree(n - 2),
+    ]
+  };
+}
+
 function App() {
+  useEffect(() => {
+    let targetElement = document.getElementById("demo-tree") as HTMLDivElement;
+    let headerElement = targetElement.querySelector("div.wb-header");
+    if (headerElement) {
+      return;
+    }
+    new Wunderbaum({
+      id: "demo",
+      element: targetElement,
+      source: [
+        createFibonacciTree(9)
+      ],
+      debugLevel: 4,
+      edit: undefined,
+      filter: undefined,
+      grid: undefined
+    });
+  });
   return (
-    <>Hello World</>
+    <div id="demo-tree"></div>
   );
 }
 
